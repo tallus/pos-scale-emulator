6501d2908cd827286cee79a468a8972933ec4857
Paul Munday <contactme@paulmunday.net>
2013-01-20 21:30:48 -0800

    added GPL as license

--- swrapper.py
+++ swrapper.py
@@ -1,33 +1,48 @@
 #!/usr/bin/env python
+
+
+#    Copyright Paul Munday 2013
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 import serial
-#import serial.tools
-#print 'available ports'
-#serial.tools.list_ports
 
+#def serial_test(device, time_out, bytes_read, flowcontrol = 'none'):
+    #'''listens to serial port [device] and prints out N [bytes_read] with timeout [time_out]'''
+    #try:
+        #ser = serial.Serial(device, 4800, parity=serial.PARITY_ODD, bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE, timeout=time_out)
+        #if flowcontrol == 'software':
+            #ser.xonxoff = 1
+        #elif  flowcontrol == 'hardware':
+            #ser.rtscts = 1
+        #buffer_input = ser.read(bytes_read)
+        #ser.close()
+        #print 'received:' + buffer_input
+        #if len(buffer_input) == 0:
+            #print 'there was no input'
+        #else:
+            #print 'Input was:' + buffer_input
+    #except serial.SerialException:
+        #print 'could not open serial device'
+        #pass
 
-def serial_test(device, time_out, bytes_read, flowcontrol = 'none'):
-    '''listens to serial port [device] and prints out N [bytes_read] with timeout [time_out]'''
-    try:
-        ser = serial.Serial(device, 4800, parity=serial.PARITY_ODD, bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE, timeout=time_out)
-        if flowcontrol == 'software':
-            ser.xonxoff = 1
-        elif  flowcontrol == 'hardware':
-            ser.rtscts = 1
-        buffer_input = ser.read(bytes_read)
-        ser.close()
-        print 'received:' + buffer_input
-        if len(buffer_input) == 0:
-            print 'there was no input'
-        else:
-            print 'Input was:' + buffer_input
-    except serial.SerialException:
-        print 'could not open serial device'
-        pass
-
-def ser_conf(device,speed = 9600, dev_parity = 'none' , byte_size = '8', stop_bitsa = '1', time_out = 'None',  flowcontrol = 'None'):
+def serial_conf(device,speed = 9600, dev_parity = 'none' , byte_size = '8', stop_bitsa = '1', time_out = 'None',  flowcontrol = 'None'):
     '''Configures a serial port for reading or writing. This is just a wrapper to make things friendlier to use, and set some sensible defaults'''
     
-    # define some dictionaries to address serial constants
+    #    define some dictionaries to address serial constants
     d_parity = { 'none' : 'serial.PARITY_NONE', 'odd' : 'serial.PARITY_ODD', 'even' : 'serial.PARITY_EVEN', 'mark' : 'serial.PARITY_MARK', 'space' : 'serial.PARITY_SPACE' } 
    d_bytesize = { '5' : 'serial.FIVEBITS', 'five' : 'FIVEBITS',  '6' : 'SIXBITS',  'six' : 'SIXBITS', '7' : 'SEVENBITS', 'seven' : 'SEVENBITS', '8' : 'EIGHTBITS', 'eight' : 'EIGHTBITS'}
    d_stopbits = { '1' : 'STOPBITS_ONE', 'one' : 'STOPBITS_', '2' : 'STOPBIT_TWO', 'two' : 'STOPBITS_TWO', '1.5' : 'STOPBITS_ONE_PONT_FIVE', 'one_point_five' : 'STOPBITS_ONE_PONT_FIVE' }
