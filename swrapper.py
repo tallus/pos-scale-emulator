83f6d59987c8ce7d37a9354ffd4a446bd1e3e75f
Paul Munday <contactme@paulmunday.net>
2013-03-18 17:31:29 -0700

    cleaned per pylint

--- swrapper.py
+++ swrapper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-
+'''Module that provides functionality to emulate pos scales with 
+   serial interfaces'''
 
 #    Copyright Paul Munday 2013
 
@@ -18,7 +19,6 @@
 
 
 import serial
-import argparse
 import random
 import ConfigParser
 import os
@@ -56,7 +56,10 @@ class Scale():
     weight_unit , k, g, lb
     """
 
-    def __init__(self, device='', speed = 9600, byte_size = '8', dev_parity = 'none', stop_bits = '1', time_out = None,  flowcontrol = 'None', terminator_byte = '', start_byte = None , weight_request='',weight_unit=''):
+    def __init__(self, device='', speed = 9600, byte_size = '8', 
+            dev_parity = 'none', stop_bits = '1', time_out = None,  
+            flowcontrol = 'None', terminator_byte = '', start_byte = None, 
+            weight_request='', weight_unit=''):
         self.device = device
         self.speed = speed
         self.byte_size = byte_size
@@ -79,13 +82,15 @@ class Scale():
         self.parity = d_parity[dev_parity.lower()]
         s_bit = str(stop_bits)
         self.stopbits = d_stopbits[s_bit.lower()]
-        self.timeout=time_out
+        self.timeout = time_out
         self.baudrate = speed
         # this wont' work as it doesn't get passed on correctly, see commented 
         # example above to h
         try:
-            self.serial_port=serial.Serial(self.device, self.baudrate, self.bytesize) #, self.parity) , self.stopbits, self.timeout, self.flowcontrol)
-            """Configures a serial port for reading or writing. This is a wrapper to make things friendlier to use, and set some sensible defaults"""
+            #Initializes  serial port. 
+            self.serial_port = serial.Serial(self.device, self.baudrate, 
+                    self.bytesize) 
+            #, self.parity) , self.stopbits, self.timeout, self.flowcontrol)
         except serial.SerialException:
             print 'could not open serial device'
         if flowcontrol == 'software':
@@ -93,7 +98,7 @@ class Scale():
         elif  flowcontrol == 'hardware':
             self.serial_port.rtscts = 1
 
-    def send_weight(self,weight):
+    def send_weight(self, weight):
         '''sends weight value to pos. N.B. Does not receive weight_request.
         Note this does not attempt to check the weight value is 
         correctly formatted. This should be implented in a wrapper
@@ -105,12 +110,14 @@ class Scale():
             send_string = weight + self.terminator_byte
         self.serial_port.write(send_string)
 
-    def read_weight(self,signal):
+    def read_weight(self, signal):
+        '''reads (weight)from the serial port on receiving 
+        the appropriate signal'''  
         self.signal = signal
         if self.signal == self.weight_request:
             self.time_out = 10
             self.serial_port.write(self.signal)
-            read_weight=bytearray()
+            read_weight = bytearray()
             while True:
                 read_byte = self.serial_port.read(1)
                 if read_byte == self.terminator_byte:
@@ -123,7 +130,8 @@ class Scale():
                 weight = str(ord(read_weight))               
             return  weight
         else:
-            raise SignalException('expected: ' + self.weight_request + 'got: ' + str(ord(self.signal)))
+            raise SignalException('expected: ' + self.weight_request 
+                    + 'got: ' + str(ord(self.signal)))
     
 #    def get_weight(self,signal):
 #        self.signal = signal
@@ -132,7 +140,7 @@ class Scale():
 #        weight = read_weight
 #        return weight
 
-    def pos_test(self,dummy_weight):
+    def pos_test(self, dummy_weight):
         '''Tests to ensure we can receive the correct command from the pos,
         and that it correctly receives the weight. Used for debugging.
         You will need to manually initiate a weight request on the pos by
@@ -144,39 +152,49 @@ class Scale():
         try:
             self.serial_port.open()
         except:
-            print 'could not open serial port'
+            print('could not open serial port')
         receive = self.serial_port.read(1)
         if receive == self.weight_request:
             self.send_weight(dummy_weight)
-            print 'sent ' + dummy_weight
+            print( 'sent ' + dummy_weight)
         else:
-            print 'expected ' + self.weight_request + '  got: ' + str(ord(receive))
+            print('expected ' + self.weight_request 
+            + '  got: ' + str(ord(receive)))
             self.serial_port.close()
 
 
 
 class Dialog(Scale):
-    """extends Scale, corresponds to built in type Dialog1. This will be used to communicate with the POS. Weight Request 0x05 (ASCII STX: Start of TeXt). Weighs in grams.  """
+    """extends Scale, corresponds to built in type Dialog1. 
+    This will be used to communicate with the POS. 
+    Weight Request 0x05 (ASCII STX: Start of TeXt). Weighs in grams.  """
     def __init__(self, device = ''):
-        Scale.__init__(self, device, 4800, '8', 'odd' ,'1', None, 'None', '\x1e', None, '\x05',  'g')
+        Scale.__init__(self, device, 4800, '8', 'odd', '1', None, 'None', 
+                '\x1e', None, '\x05', 'g')
 
 
 class Samsung(Scale):
-    """extends Scale, corresponds to built in type Samsung. This will be used to communicate with the POS. Weight Request $. Weighs in kilos."""
+    """extends Scale, corresponds to built in type Samsung. 
+    This will be used to communicate with the POS. 
+    Weight Request $. Weighs in kilos."""
     def __init__(self, device = ''):
-        Scale.__init__(self, device, 4800, '8', 'odd' ,'1', None,  'None', '\x0d', None, '$', 'k')
+        Scale.__init__(self, device, 4800, '8', 'odd', '1', None, 'None', 
+                '\x0d', None, '$', 'k')
 
 
 class Dummy(Scale):
-    """extends Scale, A dummy external scale for testing purposes. This will return a random weight between 0 & 25 kg (as kg), in 1g increments.. Weight Request $. Weighs in kilos."""
+    """extends Scale, A dummy external scale for testing purposes. 
+    This will return a random weight between 0 & 25 kg (as kg), 
+    in 1g increments.. Weight Request $. Weighs in kilos."""
     def __init__(self, device = '', weight_unit='k'):
         #Scale.__init__(self, device, 4800, '8', 'odd' ,'1', None,  'None', '\x0d', None, '$', weight_unit)
         self.weight_unit = weight_unit
     def get_weight(self):
+        '''returns random weight'''
         if self.weight_unit == 'k':
-            rand_weight = round(random.uniform(0,25),3)
+            rand_weight = round(random.uniform(0, 25), 3)
         if self.weight_unit == 'lb':
-            rand_weight = str(round(random.uniform(0,25),2)) + '0'
+            rand_weight = str(round(random.uniform(0, 25), 2)) + '0'
         else:
             raise SignalException('weight unit not defined' )
         print('returning weight: ' + rand_weight)
@@ -189,10 +207,12 @@ class Toledo(Scale):
     Returns: <STX> X1X2X3X4X5 <CR> where X1 to X5 are digits representing
     weight. No decimal point is used so it is sensible to regard kilo as 
     returning grammes."""
-    def __init__(self, device = '', speed = '',parity ='', weight_unit=''):
-        Scale.__init__(self,device, speed, '7', parity, '1', None, 'None', '\x0d', '\x02', 'W', weight_unit)
+    def __init__(self, device = '', speed = '', parity ='', weight_unit=''):
+        Scale.__init__(self, device, speed, '7', parity, '1', None, 'None', 
+                '\x0d', '\x02', 'W', weight_unit)
 
     def get_weight(self):
+        ''' return  weight read from scale'''
         read_weight = self.read_weight(self.weight_request)
         # The scale always returns a seven byte array(inc start + stop char)
         # kilo returns two digits and three decimal places, 
@@ -213,15 +233,17 @@ class Toledo(Scale):
 
 
 class AcomPC100(Scale):
-    """External scale using Toledo scale interface. Weighs in kilos(grammes) 
-    or pounds.Weight Request W. 
+    """External scale using Toledo scale interface. 
+    Weighs in kilos(grammes) or pounds.Weight Request W. 
     Returns: <STX> X1X2X3X4X5 <CR> where X1 to X5 are digits representing
     weight. No decimal point is used so it is sensible to regard kilo as 
     returning grammes."""
     def __init__(self, device = '', weight_unit=''):
-        Scale.__init__(self,device, 9600, '7', 'even', '1', None, 'None', '\x0d', '\x02', 'W', weight_unit)
+        Scale.__init__(self, device, 9600, '7', 'even', '1', None, 'None', 
+                '\x0d', '\x02', 'W', weight_unit)
 
     def get_weight(self):
+        ''' return  weight read from scale'''
         read_weight = self.read_weight(self.weight_request)
         # The scale always returns a seven byte array(inc start + stop char)
         # kilo returns two digits and three decimal places, 
@@ -241,11 +263,14 @@ class AcomPC100(Scale):
         return weight
 
 class SASI(Scale):
-    """extends Scale. External scale using SASI-RS232 scale interface. Weighs in kilos or pounds. Weight Request W """
-    def __init__(self, device = '', speed = '',parity ='', weight_unit=''):
-        Scale.__init__(self,device, speed, '7', parity, '1', None, 'None', '\x0d', '\x02', 'W', weight_unit)
+    """extends Scale. External scale using SASI-RS232 scale interface. 
+    Weighs in kilos or pounds. Weight Request W """
+    def __init__(self, device = '', speed = '', parity ='', weight_unit=''):
+        Scale.__init__(self, device, speed, '7', parity, '1', None, 'None', 
+                '\x0d', '\x02', 'W', weight_unit)
 
- def get_weight(self):
+    def get_weight(self):
+        ''' return  weight read from scale'''
         read_weight = self.read_weight(self.weight_request)
         # The Magellan always returns a seven byte array(inc start + stop char)
         # kilo returns two digits and three decimal places, 
@@ -263,11 +288,14 @@ class SASI(Scale):
 
 
 class MagellanSASI(Scale):
-    """extends Scale. External scale using SASI-RS232 scale interface. Weighs in kilos or pounds. Weight Request W """
+    """extends Scale. External scale using SASI-RS232 scale interface. 
+    Weighs in kilos or pounds. Weight Request W """
     def __init__(self, device = '', weight_unit=''):
-        Scale.__init__(self,device, 9600, '7', 'even', '1', None, 'None', '\x0d', '\x02', 'W', weight_unit)
+        Scale.__init__(self, device, 9600, '7', 'even', '1', None, 'None', 
+                '\x0d', '\x02', 'W', weight_unit)
 
     def get_weight(self):
+        ''' return  weight read from scale'''
         read_weight = self.read_weight(self.weight_request)
         # The Magellan always returns a seven byte array(inc start + stop char)
         # kilo returns two digits and three decimal places, 
@@ -284,12 +312,14 @@ class MagellanSASI(Scale):
         return weight
 
     def run_echo_test(self):
+        '''runs Magellan echo test mode. 
+        In this mode the scale will echo whatever is sent to the serial port'''
         self.serial_port.timeout = 10
         self.serial_port.open()
         print 'Beginning echo test...'
         self.serial_port.write("E")                          # enter echo mode
         start_echo = self.serial_port.read(8)                # should be E
-        start_echo
+        print start_echo
         self.serial_port.write("A")
         echo_test1 = self.serial_port.read(4)
         print echo_test1
@@ -302,17 +332,21 @@ class MagellanSASI(Scale):
         self.serial_port.close()
 
     def run_confidence_test(self):
+        '''Initiates Magellan confidence test. 
+        Returns info about state of scale's physical parts e.g. ram/rom ''' 
         self.serial_port.timeout = 10
         self.serial_port.open()
         print 'Beginning confidence test...'
-        self.serial_port.write("A")                          # initiate confidence test
-        self.serial_port.read(2)                             # read and discard two bytes
-        self.serial_port.write("B")                          # get results
-        confidence_results = self.serial_port.read(4)        # STX,?,confidence status byte,CR
+        self.serial_port.write("A")                 # initiate confidence test
+        self.serial_port.read(2)                    # read and discard two bytes
+        self.serial_port.write("B")                 # get results
+        # results: STX,?,confidence status byte,CR
+        confidence_results = self.serial_port.read(4)   
         confidence_status = bin(ord(confidence_results[3]))
-        print 'confidence results: ' + confidence_results + ' confidence status: ' + confidence_status
+        print('confidence results: ' + confidence_results 
+                + ' confidence status: ' + confidence_status)
         results_list = ['eeprom 1', 'eeprom 2', 'ram', 'processor ram', 'rom', 'not used', 'confidence test complete' ,'parity bit']                     # 1 = pass, 0 = fail
-        for i in range(0,4):
+        for i in range(0, 4):
             if confidence_status[i] == 1:
                 print results_list[i] + " test  passed"
             elif confidence_status[i] == 0:
@@ -320,18 +354,21 @@ class MagellanSASI(Scale):
                 flag = 0
 
             if flag: 
-                print "something is wrong, the scale will not work until it is fixed"
+                print('something is wrong, the scale will not work until it is fixed')
         self.serial_port.close()
 
 class SignalException(Exception):
+    '''used to throw exceptions and print user readable error messages 
+    without printing out traceback'''
     pass
 
 # FUNCTIONS
 
 def pos_test_samsung(s_port):
+    '''run tests to make sure samsung scale in pos is working'''
     print('starting tests')
     print('opening serial port')
-    opos_scale=Samsung(s_port)
+    opos_scale = Samsung(s_port)
     print('start test1')
     opos_scale.pos_test('5')
     print('end test1')
@@ -345,20 +382,21 @@ def pos_test_samsung(s_port):
     opos_scale.pos_test('56.78')   # pounds
     print('end test4')
     # generate random float between 0 & 10 and round to 3 decimal places
-    rand_weight=round(random.uniform(0,10),3)
+    rand_weight = round(random.uniform(0, 10), 3)
     print('start test5')
     opos_scale.pos_test(str(rand_weight))
     print('end test5')
 
 def pos_test_dialog(s_port):
+    '''run tests to make sure dialog scale in pos is working'''
     print('starting tests')
     print('opening serial port')
-    opos_scale=Dialog(s_port)
+    opos_scale = Dialog(s_port)
     print('start test1')
     opos_scale.pos_test('500')
     print('end test1')
     # generate random float between 0 & 10000, rounded to whole units
-    rand_weight=round(random.uniform(0,10000))
+    rand_weight = round(random.uniform(0, 10000))
     print('start test2')
     print('end test2')
     opos_scale.pos_test(str(rand_weight))
@@ -367,23 +405,27 @@ def pos_test_dialog(s_port):
     print('end test3')
 
 def scale_test_magellan(s_port):
+    '''runs tests on external Magellan scale'''
     print('starting tests')
-    test_scale=MagellanSASI(s_port)
+    test_scale = MagellanSASI(s_port)
     test_scale.run_echo_test()
     test_scale.run_confidence_test()
     print('end tests')
 
 def get_config(section):
+    '''Reads in configuration file. 
+    This provides attributes for the scale setup'''
     config = ConfigParser.ConfigParser()
-    config.read([os.path.expanduser('~/.swrapper.cfg'),'/etc/swrapper.cfg','swrapper.cfg'])
+    config.read([os.path.expanduser('~/.swrapper.cfg'), 
+        '/etc/swrapper.cfg', 'swrapper.cfg'])
     if config.has_section('pos'):
-        pos_values=dict(config.items('pos'))
+        pos_values = dict(config.items('pos'))
     if config.has_section('scale'):
-        scales_values=dict(config.items('scale'))
+        scales_values = dict(config.items('scale'))
     if config.has_option('scale', 'type'):
-        defined_scale=config.get('scale', 'type')
+        defined_scale = config.get('scale', 'type')
         if config.has_section(defined_scale):
-            scale_definition=dict(config.items(defined_scale))
+            scale_definition = dict(config.items(defined_scale))
     if section == 'pos':
         if pos_values:
             return pos_values
@@ -397,15 +439,19 @@ def get_config(section):
 # Unit Tests
 
 class MyUnitTest(unittest.TestCase):
+    '''unit tests for non class functions'''
     def test_config_pos(self):
+        '''reads config file (unedited) for pos scale type'''
         pos_config = get_config('pos')
         self.assertEqual(pos_config['type'], 'samsung')
 
     def test_config_scale(self):
+        '''reads config file (unedited) for external scale type'''
         scale_config = get_config('scale')
         self.assertEqual(scale_config['type'], 'example')
 
     def test_defined_scale(self):
+        '''reads config file (unedited) for example scale definitions'''
         defined_scale = get_config('scale_definition')
         self.assertEqual(defined_scale['speed'], '9600')
 
